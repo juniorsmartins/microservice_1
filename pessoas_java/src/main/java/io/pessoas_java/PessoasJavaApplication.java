5797b9a37cc31d3865cf7e483b43de3b7c15ba31
juniorsmartins <juniorsoaresmartins@gmail.com>
2023-10-01 09:52:07 -0400

    FEAT: add projeto

---
+++ pessoas_java/src/main/java/io/pessoas_java/PessoasJavaApplication.java
@@ -0,0 +1,13 @@
+package io.pessoas_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PessoasJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PessoasJavaApplication.class, args);
+	}
+
+}
---
+++ pessoas_java/src/test/java/io/pessoas_java/PessoasJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pessoas_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PessoasJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
