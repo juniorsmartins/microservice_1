1f2da9fac2dd58477cdf2a46d5e1fa45ca3eae98
juniorsmartins <juniorsoaresmartins@gmail.com>
2023-10-17 18:19:55 -0400

    FEAT: add parâmetros para Pesquisa

--- pessoas_java/src/main/java/io/pessoas_java/adapters/out/spec/PessoaSpec.java
+++ pessoas_java/src/main/java/io/pessoas_java/adapters/out/spec/PessoaSpec.java
@@ -33,6 +33,40 @@ public final class PessoaSpec {
                 predicados.add(criteriaBuilder.or(parametrosPredicados.toArray(new Predicate[0])));
             }
 
+            if (ObjectUtils.isNotEmpty(filtro.getSobrenome())) {
+                var parametros = Arrays.asList(filtro.getSobrenome().split(","));
+                List<Predicate> parametrosPredicados = parametros.stream()
+                    .map(parametro -> criteriaBuilder.like(
+                        criteriaBuilder.lower(root.get("sobrenome")), "%" + parametro.toLowerCase() + "%"))
+                    .toList();
+                predicados.add(criteriaBuilder.or(parametrosPredicados.toArray(new Predicate[0])));
+            }
+
+            if (ObjectUtils.isNotEmpty(filtro.getCpf())) {
+                var parametros = Arrays.asList(filtro.getCpf().split(","));
+                List<Predicate> parametrosPredicados = parametros.stream()
+                    .map(parametro -> criteriaBuilder.like(
+                        criteriaBuilder.lower(root.get("cpf")), "%" + parametro.toLowerCase() + "%"))
+                    .toList();
+                predicados.add(criteriaBuilder.or(parametrosPredicados.toArray(new Predicate[0])));
+            }
+
+            if (ObjectUtils.isNotEmpty(filtro.getSexo())) {
+                predicados.add(criteriaBuilder.equal(root.get("sexo"), filtro.getSexo()));
+            }
+
+            if (ObjectUtils.isNotEmpty(filtro.getGenero())) {
+                predicados.add(criteriaBuilder.equal(root.get("genero"), filtro.getGenero()));
+            }
+
+            if (ObjectUtils.isNotEmpty(filtro.getNivelEducacional())) {
+                predicados.add(criteriaBuilder.equal(root.get("nivelEducacional"), filtro.getNivelEducacional()));
+            }
+
+            if (ObjectUtils.isNotEmpty(filtro.getNacionalidade())) {
+                predicados.add(criteriaBuilder.equal(root.get("nacionalidade"), filtro.getNacionalidade()));
+            }
+
             return criteriaBuilder.and(predicados.toArray(new Predicate[0]));
         }));
     }
