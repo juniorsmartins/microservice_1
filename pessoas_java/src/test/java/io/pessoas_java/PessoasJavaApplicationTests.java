2bae9afdf3230e4e86ac910c989a363cb085bb87
juniorsmartins <juniorsoaresmartins@gmail.com>
2023-10-25 17:28:01 -0400

    TEST: concluído teste de Swagger com Testcontainers.

--- pessoas_java/src/test/java/io/pessoas_java/PessoasJavaApplicationTests.java
+++ pessoas_java/src/test/java/io/pessoas_java/PessoasJavaApplicationTests.java
@@ -1,10 +1,11 @@
 package io.pessoas_java;
 
+import io.pessoas_java.integration.AbstractIntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PessoasJavaApplicationTests {
+class PessoasJavaApplicationTests extends AbstractIntegrationTest {
 
 	@Test
 	void contextLoads() { }
