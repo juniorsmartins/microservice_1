8e18794b076d70ef25b3e5c252cf8186c67d994f
juniorsmartins <juniorsoaresmartins@gmail.com>
2023-10-29 10:04:28 -0400

    FEAT: add Enum Role, entity User, Repository User e tabela user.

---
+++ pessoas_java/src/main/java/io/pessoas_java/config/security/entity/UserEntity.java
@@ -0,0 +1,74 @@
+package io.pessoas_java.config.security.entity;
+
+import io.pessoas_java.config.security.entity.value_object.UserRole;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = {"id"})
+public final class UserEntity implements Serializable, UserDetails {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private String id;
+
+    @Column(name = "login", length = 150, unique = true, nullable = false)
+    private String login;
+
+    @Column(name = "password", length = 512, nullable = false)
+    private String password;
+
+    @Column(name = "role", nullable = false)
+    private UserRole role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        if (this.role == UserRole.ADMIN)
+            return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
+
---
+++ pessoas_java/src/main/java/io/pessoas_java/config/security/entity/value_object/UserRole.java
@@ -0,0 +1,18 @@
+package io.pessoas_java.config.security.entity.value_object;
+
+public enum UserRole {
+
+    ADMIN("Admin"),
+    USER("User");
+
+    private String role;
+
+    UserRole(String role) {
+        this.role = role;
+    }
+
+    public String getRole() {
+        return this.role;
+    }
+}
+
