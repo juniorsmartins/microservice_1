6a2f597df3c0c0259512116d649d4fa55bc95fa4
juniorsmartins <juniorsoaresmartins@gmail.com>
2023-10-29 17:27:21 -0400

    FEAT: add SecurityFilter, TokenService e etc.

--- pessoas_java/src/main/java/io/pessoas_java/config/security/config/SecurityConfiguration.java
+++ pessoas_java/src/main/java/io/pessoas_java/config/security/config/SecurityConfiguration.java
@@ -1,5 +1,7 @@
 package io.pessoas_java.config.security.config;
 
+import io.pessoas_java.config.security.service.SecurityFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -12,10 +14,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Autowired
+    private SecurityFilter securityFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -30,6 +36,7 @@ public class SecurityConfiguration {
                     .hasRole("ADMIN")
                     .anyRequest()
                     .authenticated())
+            .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
             .build();
     }
 
