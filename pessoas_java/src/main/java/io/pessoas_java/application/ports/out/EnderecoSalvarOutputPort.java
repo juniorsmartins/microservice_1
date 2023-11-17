c67645ab8b5f1e1c7ceebd984d052d6558c3d24c
juniorsmartins <juniorsoaresmartins@gmail.com>
2023-11-17 06:06:31 -0400

    REFACTOR: aplicação de Yagni.

--- pessoas_java/src/main/java/io/pessoas_java/application/ports/out/EnderecoSalvarOutputPort.java
+++
@@ -1,9 +0,0 @@
-package io.pessoas_java.application.ports.out;
-
-import io.pessoas_java.application.core.domain.value_object.Endereco;
-
-public interface EnderecoSalvarOutputPort {
-
-    Endereco salvar(Endereco endereco);
-}
-
