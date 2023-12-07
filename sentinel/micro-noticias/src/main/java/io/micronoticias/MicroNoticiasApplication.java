96446e57d7ba40d1c476a4c78fbbb5b071f2f028
juniorsmartins <juniorsoaresmartins@gmail.com>
2023-12-07 18:37:28 -0400

    FEAT: start project

---
+++ sentinel/micro-noticias/src/main/java/io/micronoticias/MicroNoticiasApplication.java
@@ -0,0 +1,13 @@
+package io.micronoticias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroNoticiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroNoticiasApplication.class, args);
+	}
+
+}
---
+++ sentinel/micro-noticias/src/test/java/io/micronoticias/MicroNoticiasApplicationTests.java
@@ -0,0 +1,13 @@
+package io.micronoticias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroNoticiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
