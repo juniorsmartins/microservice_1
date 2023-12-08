f646338ec614c5ef871924a0babe0345620df645
juniorsmartins <juniorsoaresmartins@gmail.com>
2023-12-08 19:42:39 -0400

    FEAT: add microsserviço Pessoas

---
+++ sentinel/micro-pessoas/src/test/java/io/micropessoas/MicroPessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package io.micropessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroPessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
