4a675cfdfdf1011af8dc8ba45e80a8889e51e037
juniorsmartins <juniorsoaresmartins@gmail.com>
2023-12-22 19:47:04 -0400

    TEST: add

--- sentinel/micro-noticias/src/test/java/io/micronoticias/adapter/in/dto/request/NoticiaCriarDtoInTest.java
+++ sentinel/micro-noticias/src/test/java/io/micronoticias/adapter/in/dto/request/NoticiaCriarDtoInTest.java
@@ -1,5 +1,6 @@
 package io.micronoticias.adapter.in.dto.request;
 
+import com.github.javafaker.Faker;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validator;
 import org.junit.jupiter.api.*;
@@ -20,6 +21,8 @@ import java.util.Set;
 @ExtendWith({SpringExtension.class, MockitoExtension.class})
 @DisplayName("Classe NotíciaCriarDtoIn")
 class NoticiaCriarDtoInTest {
+
+    public static Faker faker = new Faker();
 
     @Autowired
     private Validator validator;
@@ -55,6 +58,44 @@ class NoticiaCriarDtoInTest {
         @DisplayName("com tamanho inválido")
         void dadoChapeuComMaisCaracteresQuePermitido_QuandoValidar_EntaoLancarException() {
             var dtoIn = new NoticiaCriarDtoIn("21_caracteres--------", "Titulo", "Linha Fina", "Lide", "Corpo", "Nome Autor", "Fonte");
+            Set<ConstraintViolation<NoticiaCriarDtoIn>> violations = validator.validate(dtoIn);
+            Assertions.assertFalse(violations.isEmpty());
+            Assertions.assertEquals(1, violations.size());
+        }
+    }
+
+    @Nested
+    @DisplayName("Titulo")
+    @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+    class Titulo {
+
+        @Test
+        @Order(1)
+        @DisplayName("nulo")
+        void dadoTituloNulo_QuandoValidar_EntaoLancarException() {
+            var dtoIn = new NoticiaCriarDtoIn("Chapéu", null, "Linha Fina", "Lide", "Corpo", "Nome Autor", "Fonte");
+            Set<ConstraintViolation<NoticiaCriarDtoIn>> violations = validator.validate(dtoIn);
+            Assertions.assertFalse(violations.isEmpty());
+            Assertions.assertEquals(1, violations.size());
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {"", "   "})
+        @Order(2)
+        @DisplayName("vazio ou em branco")
+        void dadoTituloValorVazio_QuandoValidar_EntaoLancarException(String titulo) {
+            var dtoIn = new NoticiaCriarDtoIn("Chapéu", titulo, "Linha Fina", "Lide", "Corpo", "Nome Autor", "Fonte");
+            Set<ConstraintViolation<NoticiaCriarDtoIn>> violations = validator.validate(dtoIn);
+            Assertions.assertFalse(violations.isEmpty());
+            Assertions.assertEquals(1, violations.size());
+        }
+
+        @Test
+        @Order(3)
+        @DisplayName("com tamanho inválido")
+        void dadoTituloComMaisCaracteresQuePermitido_QuandoValidar_EntaoLancarException() {
+            var titulo = faker.lorem().characters(101, 150);
+            var dtoIn = new NoticiaCriarDtoIn("Chapéu", titulo, "Linha Fina", "Lide", "Corpo", "Nome Autor", "Fonte");
             Set<ConstraintViolation<NoticiaCriarDtoIn>> violations = validator.validate(dtoIn);
             Assertions.assertFalse(violations.isEmpty());
             Assertions.assertEquals(1, violations.size());
